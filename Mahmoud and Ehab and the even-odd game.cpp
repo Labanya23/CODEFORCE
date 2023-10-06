0f7798833fa4a3fb52cdaea79545866ed6c540d3
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-10-06 15:12:24 +0600

    Add files via upload

---
+++ Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Mahmoud"<<endl;
+    }
+    else{
+        cout<<"Ehab"<<endl;
+    }
+    return 0;
+}
