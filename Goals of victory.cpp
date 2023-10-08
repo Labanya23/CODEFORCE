6a9c31dfac5e44c7a9f581c054ef700520b3b2fb
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-10-08 16:29:06 +0600

    Add files via upload

---
+++ Goals of victory.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--)
+    {
+        int i,a,n,sum=0;
+        char b;
+        cin>>n;
+        int arr[n];
+        for( i=1;i<n-1;i++){
+            cin>>arr[i];
+            cin>>b;
+
+         }
+         sum+=arr[i];
+         cout<<sum<<endl;
+    }
+    return 0;
+}
