949938570e41da2df301bd30ec8e849c3b709b3a
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2024-08-18 11:29:01 +0600

    Add files via upload

---
+++ scorrer.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int x1,x2,y1,y2;
+        cin>>x1>>x2>>y1>>y2;
+        if(x1>x2){
+            swap(x1,x2);
+            swap(y1,y2);
+        }
+        if(y2>=y1){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+    return 0;
+}
